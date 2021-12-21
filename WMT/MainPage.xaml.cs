f3d589ce39dc67a05e69177b7c18956f5133c4fd
Daniel Scott <Shnooks741@gmail.com>
2021-12-21 07:29:20 -0700

    Add files via upload

---
+++ WMT/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WMT
+{
+    public partial class MainPage : ContentPage
+    {
+        public Dictionary<string, string> hToolDict = new Dictionary<string, string>() { {"1234", "Test" } };
+        public Dictionary<string, string> aToolDict = new Dictionary<string, string>() { { "1234", "Test" } };
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void infoPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new infoPage());
+        }
+
+        public void toolLookUp(object sender, EventArgs e)
+        {
+            string key = uInput.Text;
+            string handTool = ""; 
+            string applicator = "";
+
+            foreach(KeyValuePair<string, string> x in hToolDict)
+            {
+                if (x.Key == key){
+                    handTool = x.Value;
+                }
+                else
+                {
+                    handTool = "N/A";
+                }
+            }
+
+            foreach (KeyValuePair<string, string> x in aToolDict)
+            {
+                if (x.Key == key)
+                {
+                    applicator = x.Value;
+                }
+                else
+                {
+                    applicator = "N/A";
+                }
+            }
+
+            hTool.Text = handTool;
+            aTool.Text = applicator;
+        }
+    }
+}
